271274625ce71516df874905bcf91358967e6de3
GioCG <gcarrera-2020313@kinal.edu.gt>
2025-03-11 23:46:40 -0600

    se termino de pulir todo con mas validaciones

--- src/buys/purchaseHistory/purchaseHistory.contrroller.js
+++ src/buys/purchaseHistory/purchaseHistory.contrroller.js
@@ -1,24 +1,33 @@
-import PurchaseHistory from "../purchaseHistory/purchaseHistory.model.js"; 
+import Bill from "../bill/bill.model.js";
+import jwt from "jsonwebtoken";
 
 export const listPurchaseHistory = async (req, res) => {
-  const userId = req.user.id; 
- 
+  const token = req.header("x-token"); 
+
   try {
-    const history = await PurchaseHistory.find({ user: userId })
+    const { uid } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
+
+    const bills = await Bill.find({ user: uid })
       .populate({
-        path: "bill", 
-        populate: {
-          path: "items.product", 
-          model: "Product",
-        },
+        path: "items.product",
+        model: "Product",
+        select: "name price image", 
       })
       .sort({ date: -1 }); 
- 
-    if (history.length === 0) {
+
+    if (bills.length === 0) {
       return res.status(404).json({ message: "No se encontró historial de compras" });
     }
- 
-    res.status(200).json(history);
+
+    const formattedBills = bills.map((bill, index) => {
+      return {
+        purchase: `Purchase #${index + 1}`, 
+        total: bill.total,
+        items: bill.items, 
+      };
+    });
+
+    res.status(200).json(formattedBills);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
--- src/buys/purchaseHistory/purchaseHistory.routs.js
+++ src/buys/purchaseHistory/purchaseHistory.routs.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { listPurchaseHistory } from "../purchaseHistory/purchaseHistory.contrroller.js";
- 
+import {validarJWT} from "../../middlewares/validar-jwt.js";
+
 const router = express.Router();
  
-router.get("/", listPurchaseHistory);
+router.get("/",validarJWT, listPurchaseHistory);
  
 export default router;
